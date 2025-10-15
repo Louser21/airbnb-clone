20f88442998ecff5811e20e0e687cc6a4e175e5d
Nitin Bhandari <nitin211206@gmail.com>
2025-10-15 15:33:46 +0530

    feat: Listings -3 Category: Info

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,36 +1,55 @@
 "use client";
 
 import L from "leaflet";
-import { MapContainer, Marker,TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { useEffect } from "react";
 
-//@ts-expect-error: OSC so error might occur
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-    iconUrl : markerIcon.src,
-    iconRetinaUrl : markerIcon2x.src,
-    shadowUrl : markerShadow.src
+const icon = L.icon({
+  iconUrl: "/leaflet/marker-icon.png",
+  iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+  shadowUrl: "/leaflet/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
 });
 
+
 interface MapProps {
-    center? : number[],
+  center?: number[],
+}
+
+function ScrollZoom() {
+  const map = useMap();
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const zoom = Math.min(10, 2 + scrollY / 200);
+      map.setZoom(zoom);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [map]);
+  return null;
 }
 
 
-const Map : React.FC<MapProps> = ({center}) => {
+const Map: React.FC<MapProps> = ({ center }) => {
   return (
-        <MapContainer
-        center={center as L.LatLngExpression || [51,-0.09]}
-        zoom={center ? 4 : 2}
-        scrollWheelZoom = {false}
-        className="h-[35vh] rounded-lg"
-        >
-
-        </MapContainer>
+    <MapContainer
+      center={center as L.LatLngExpression || [51, -0.09]}
+      zoom={center ? 4 : 1}
+      scrollWheelZoom={false}
+      className="h-[35vh] rounded-lg"
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {center && (
+        <Marker position={center as L.LatLngExpression} icon={icon}/>
+      )}
+      <ScrollZoom />
+    </MapContainer>
   )
 }
 
--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import React, { useMemo, useState } from 'react'
-import Modal from './Modal'
-import useRentModal from '@/app/hooks/useRentModal'
-import Heading from '../Heading';
-import { categories } from '../Navbar/Categories';
-import CategoryInput from '../Inputs/CategoryInput';
-import { FieldValues, useForm } from 'react-hook-form';
-import CountrySelect from '../Inputs/CountrySelect';
-import Map from '../Map';
+import React, { useMemo, useState } from "react";
+import Modal from "./Modal";
+import useRentModal from "@/app/hooks/useRentModal";
+import Heading from "../Heading";
+import { categories } from "../Navbar/Categories";
+import CategoryInput from "../Inputs/CategoryInput";
+import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../Inputs/CountrySelect";
+import dynamic from "next/dynamic";
+import Counter from "../Inputs/Counter";
 
 enum STEPS {
     CATEGORY = 0,
@@ -16,11 +17,10 @@ enum STEPS {
     INFO,
     IMAGES,
     DESCRIPTION,
-    PRICE
+    PRICE,
 }
 
 const RentModal = () => {
-     
     const rentModal = useRentModal();
 
     const [step, setStep] = useState(STEPS.CATEGORY);
@@ -30,10 +30,8 @@ const RentModal = () => {
         handleSubmit,
         setValue,
         watch,
-        formState: {
-            errors,
-        },
-        reset
+        formState: { errors },
+        reset,
     } = useForm<FieldValues>({
         defaultValues: {
             category: "",
@@ -41,85 +39,116 @@ const RentModal = () => {
             guestCount: 1,
             roomCount: 1,
             bathroomCount: 1,
-            imageSrc: '',
+            imageSrc: "",
             price: 1,
-            title: '',
+            title: "",
             description: "",
-        }
+        },
     });
 
-    const category = watch('category');
-    const location = watch('location');
+    const category = watch("category");
+    const location = watch("location");
+    const guestCount = watch("guestCount");
+    const roomCount = watch("roomCount");
+    const bathroomCount = watch("bathroomCount");
+
+    const Map = useMemo(() => dynamic(() => import("../Map"), { ssr: false }), [location])
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
             shouldDirty: true,
             shouldTouch: true,
             shouldValidate: true,
-        })
-    }
+        });
+    };
 
     const onBack = () => {
-        setStep(val => val - 1)
-    }
+        setStep((val) => val - 1);
+    };
 
     const onNext = () => {
-        setStep(val => val + 1);
-    }
+        setStep((val) => val + 1);
+    };
 
     const actionLabel = useMemo(() => {
         if (step === STEPS.PRICE) {
             return "Create";
         }
         return "Next";
-    }, [step])
+    }, [step]);
 
     const secondaryActionLabel = useMemo(() => {
         if (step === STEPS.CATEGORY) {
             return undefined;
         }
-        return "Back"
-    }, [step])
+        return "Back";
+    }, [step]);
 
     let bodyContent = (
-        <div className='flex flex-col gap-8'>
+        <div className="flex flex-col gap-8">
             <Heading
                 title="Which of these best describes your place"
-                subtitle='Pick a category'
+                subtitle="Pick a category"
             />
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto '>
-                {categories.map(item => {
-                    return <div key={item.label} className='col-span-1'>
-                        <CategoryInput
-                            onClick={category => setCustomValue('category', category)}
-                            selected={category === item.label}
-                            label={item.label}
-                            icon={item.icon}
-                        />
-                    </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto ">
+                {categories.map((item) => {
+                    return (
+                        <div key={item.label} className="col-span-1">
+                            <CategoryInput
+                                onClick={(category) => setCustomValue("category", category)}
+                                selected={category === item.label}
+                                label={item.label}
+                                icon={item.icon}
+                            />
+                        </div>
+                    );
                 })}
             </div>
         </div>
-    )
+    );
 
     if (step === STEPS.LOCATION) {
         bodyContent = (
-            <div className='flex flex-col gap-8 '>
+            <div className="flex flex-col gap-8 ">
                 <Heading
-                    title='Where is your place located'
-                    subtitle='Help guests find you!'
+                    title="Where is your place located"
+                    subtitle="Help guests find you!"
                 />
                 <CountrySelect
-                value={location}
-                onChange={(value)=>setCustomValue('location',value)}
+                    value={location}
+                    onChange={(value) => setCustomValue("location", value)}
                 />
-                <Map />
+                <Map center={location?.latlng} />
+            </div>
+        );
+    }
+
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading title="Share some basics about your place" subtitle="What amenities do you have?" />
+                <Counter title="Guests" subtitle="How many guests do you allow?" value={guestCount} onChange={(value) => setCustomValue('guestCount', value)} />
+                <hr />
+                <Counter title="Rooms" subtitle="How many rooms do you have?" value={roomCount} onChange={(value) => setCustomValue('roomCount', value)} />
+                <hr />
+                <Counter title="Bathrooms" subtitle="How many bathrooms do you have?" value={bathroomCount} onChange={(value) => setCustomValue('bathroomCount', value)} />
+                <hr />
             </div>
         )
     }
-    return (
-        <Modal body={bodyContent} title='Airbnb Your Home!' isOpen={rentModal.isOpen} onClose={rentModal.onClose} onSubmit={onNext} actionLabel={actionLabel} secondaryLabel={secondaryActionLabel} secondaryAction={step === STEPS.CATEGORY ? undefined : onBack} />
-    )
-}
 
-export default RentModal
+    return (
+        <Modal
+            body={bodyContent}
+            title="Airbnb Your Home!"
+            isOpen={rentModal.isOpen}
+            onClose={rentModal.onClose}
+            onSubmit={onNext}
+            actionLabel={actionLabel}
+            secondaryLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+        />
+    );
+};
+
+export default RentModal;
